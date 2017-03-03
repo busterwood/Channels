16f7a0eb3662553b6c9e2bab527763bdcf163834
BusterWood <busterwood@users.noreply.github.com>
2017-03-03 14:30:20 +0000

    Add timeout.after which returns a channel

---
+++ src/BusterWood.Channels/Timeout.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BusterWood.Channels
+{
+    public static class Timeout
+    {
+        public static Channel<DateTime> After(TimeSpan timeout)
+        {
+            var c = new Channel<DateTime>();
+            Task.Delay(timeout).ContinueWith(_ => c.SendAsync(DateTime.UtcNow));
+            return c;
+        }
+    }
+}
